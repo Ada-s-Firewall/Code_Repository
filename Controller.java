8d90b97e0d0324ca8647a280e42aeb88f9b9e690
Will Higdon <47529524+YaBOIWill@users.noreply.github.com>
2020-02-24 23:15:29 -0500

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,24 +1,24 @@
-
-//test controller
-
+/*
+*
+* This file is a controller for displaying the user's information on their
+* profile when they login.
+*
+* @author Will Higdon  Last Updated: 2/24/2020
+*/
 public class Controller {
    public static void main(String[] args) {
-
-      //fetch user record based on their info from the database
+      //Fetches user record based on their info from the database
       User model  = retrieveUserFromDatabase();
-
-      //Creates a view to write the user's details onto the console
+      //Creates a view to display the user's infor on their profile
       UserView view = new UserView();
-
       UserController controller = new UserController(model, view);
-
       controller.updateView();
-      
    }
 
-   // User test case, retrieves the user's info from the Database
+   // User test case, an example of the user's info.
    private static User retrieveUserFromDatabase(){
       User user = new User();
+      // User info
       user.setUserName("WigginsDough");
       user.setUserPassword("69isGOAT");
       user.setUserEmail("dwiggins@yikes.com");
@@ -30,13 +30,14 @@ public class Controller {
 
 // View class for the User
 class UserView {
-   public void printUserDetails(String userName, String userPassword, String userEmail, String userFirstName, String userLastName){
-      System.out.println("USER INFO: ");
-      System.out.println("Username: " + userName);
-      System.out.println("Password: " + userPassword);
-      System.out.println("Email Address: " + userEmail);
-      System.out.println("First Name: " + userFirstName);
-      System.out.println("Last Name: " + userLastName);
+   public void printUserDetails(String _userName, String _userPassword, String _userEmail, String _userFirstName, String _userLastName){
+      System.out.println("My Info: ");
+      System.out.println("");
+      System.out.println("Username: " + _userName);
+      System.out.println("Password: " + _userPassword);
+      System.out.println("Email Address: " + _userEmail);
+      System.out.println("First Name: " + _userFirstName);
+      System.out.println("Last Name: " + _userLastName);
    }
 }
 
@@ -47,49 +48,49 @@ class User {
    private String userEmail;
    private String userFirstName;
    private String userLastName;
-   
+
    //============== GETTERS ==================
-   
+
    public String getUserName() {
-      return userName;
+      return this.userName;
    }
-   
+
    public String getUserPassword() {
-      return userPassword;
+      return this.userPassword;
    }
-   
+
    public String getUserEmail() {
-      return userEmail;
+      return this.userEmail;
    }
-   
+
    public String getUserFirstName() {
-      return userFirstName;
+      return this.userFirstName;
    }
-   
+
    public String getUserLastName() {
-      return userLastName;
+      return this.userLastName;
    }
-   
-   //============== SETTERS ==================
-   
-   public void setUserName(String userName) {
-      this.userName = userName;
+
+   //=============== SETTERS ==================
+
+   public void setUserName(String _userName) {
+      this.userName = _userName;
    }
-   
-   public void setUserPassword(String userPassword) {
-      this.userPassword = userPassword;
+
+   public void setUserPassword(String _userPassword) {
+      this.userPassword = _userPassword;
    }
-   
-   public void setUserEmail(String userEmail) {
-      this.userEmail = userEmail;
+
+   public void setUserEmail(String _userEmail) {
+      this.userEmail = _userEmail;
    }
-   
-   public void setUserFirstName(String userFirstName) {
-      this.userFirstName = userFirstName;
+
+   public void setUserFirstName(String _userFirstName) {
+      this.userFirstName = _userFirstName;
    }
-   
-   public void setUserLastName(String userLastName) {
-      this.userLastName = userLastName;
+
+   public void setUserLastName(String _userLastName) {
+      this.userLastName = _userLastName;
    }
 }
 
@@ -98,57 +99,57 @@ class UserController {
    private User model;
    private UserView view;
 
-   public UserController(User model, UserView view){
-      this.model = model;
-      this.view = view;
+   public UserController(User _model, UserView _view){
+      this.model = _model;
+      this.view = _view;
    }
 
-   //================= GETTERS =====================
-  
+   //=================  GETTERS ===============
+
    public String getUserName(){
-      return model.getUserName();		
+      return this.model.getUserName();
    }
 
    public String getUserPassword(){
-      return model.getUserPassword();		
+      return this.model.getUserPassword();
    }
-   
+
    public String getUserEmail(){
-      return model.getUserEmail();		
+      return this.model.getUserEmail();
    }
-   
+
    public String getUserFirstName(){
-      return model.getUserFirstName();		
+      return this.model.getUserFirstName();
    }
-   
+
    public String getUserLastName(){
-      return model.getUserFirstName();		
-   }
-   
-   //================= SETTERS =====================
-   
-   public void setUserName(String userName){
-      model.setUserName(userName);		
-   }
-   
-   public void setUserPassword(String userPassword){
-      model.setUserPassword(userPassword);		
+      return this.model.getUserFirstName();
    }
 
-   public void setUserEmail(String userEmail){
-      model.setUserEmail(userEmail);		
-   }
-   
-   public void setUserFirstName(String userFirstName){
-      model.setUserFirstName(userFirstName);		
+   //=================  SETTERS ===============
+
+   public void setUserName(String _userName){
+      model.setUserName(_userName);
    }
 
-   public void setUserLastName(String userLastName){
-      model.setUserLastName(userLastName);		
+   public void setUserPassword(String _userPassword){
+      model.setUserPassword(_userPassword);
+   }
+
+   public void setUserEmail(String _userEmail){
+      model.setUserEmail(_userEmail);
+   }
+
+   public void setUserFirstName(String _userFirstName){
+      model.setUserFirstName(_userFirstName);
+   }
+
+   public void setUserLastName(String _userLastName){
+      model.setUserLastName(_userLastName);
    }
 
    //Updates the View
-   public void updateView(){				
+   public void updateView(){
       view.printUserDetails(model.getUserName(), model.getUserPassword(), model.getUserEmail(), model.getUserFirstName(), model.getUserLastName());
-   }	
+   }
 }
