98e213dd14f2d11bff69985b69ef3d9b7d6c53d1
Will Higdon <47529524+YaBOIWill@users.noreply.github.com>
2020-03-30 23:16:07 -0400

    starting creating necessary models

---
+++ Models/CheckEmail.java
@@ -0,0 +1,16 @@
+package Models;
+
+/*
+check email model, will update later once finished
+
+@author: Will Higdon
+Last Updated: March 30, 2020
+*/
+
+/*
+Import Statements Here
+*/
+
+public class CheckEmail {
+    //code here
+}
---
+++ Models/GetUserMusicList.java
@@ -0,0 +1,16 @@
+package Models;
+
+/*
+* gets the user's full music list model, will update later once finished
+*
+* @author: Will Higdon
+* Last Updated: March 30, 2020
+*/
+
+/*
+Import Statements Here
+*/
+
+public class GetUserMusicList {
+    //code here
+}
