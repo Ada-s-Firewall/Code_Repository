9bea99f4c9dfcc2e058c07f383a44f1eab62dbac
Will Higdon <47529524+YaBOIWill@users.noreply.github.com>
2020-03-31 23:20:04 -0400

    added variables and import statements

--- Models/UserMusicList.java
+++ Models/UserMusicList.java
@@ -4,13 +4,24 @@ package Models;
 * Model for the user's full music list.
 *
 * @author: Will Higdon
-* Last Updated: March 30, 2020
+* Last Updated: March 31, 2020
 */
 
-/*
-Import Statements Here
-*/
+import Objects.AlbumObject;
+import Objects.ArtistObject;
+import Objects.TrackObject;
+import Objects.MusicObject;
+import Objects.MusicObjectList;
+import java.util.ArrayList;
 
 public class UserMusicList {
-    //code here
+    /*
+    * Variables for the user's music list.
+    */
+    private int usersAlbumsCompleted;
+    private int usersAlbumsPlanToListen;
+    ArrayList<String> usersFavoriteAlbums = new ArrayList<String>();
+    ArrayList<String> usersFavoriteArtists = new ArrayList<String>();
+    ArrayList<String> usersFavoriteTracks = new ArrayList<String>();
+    
 }
