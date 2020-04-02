b666fb9e84b3aab2e310ece811d7a323c40ec579
Quinn <qatjinas@uncg.edu>
2020-04-02 17:52:29 -0400

    added files to objects

--- Objects/RatingObject.java
+++ Objects/RatingObject.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Objects;
 
 /**
  *
  * @author Quinn Tjin-A-Soe
+ * Last updated: 04.02.2020
  */
 public class RatingObject {
     
--- Objects/RecordObject.java
+++ Objects/RecordObject.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Objects;
+
+import java.util.UUID;
 
 /**
  *
- * @author Quinn Tjin-A-Soe
+ * @author Quinn Tjin-A-Soe Last updated 04.02.2020
  */
 public class RecordObject {
+
+    protected String uuid;
+    protected String name;
+    protected String id;
+    protected boolean active = true;
+// ================================ CONSTRUCTORS ====================================
+
+    public RecordObject(String uuid, String name, String id) {
+        this.uuid = uuid;
+        this.name = name;
+        this.id = id;
+    }
     
+// ================================ METHODS ====================================
+    public void makeActive() {
+        this.active = true;
+    }
+
+    public void makeInactive() {
+        this.active = false;
+    }
+
+    protected static String generateUuid() {
+        return UUID.randomUUID().toString();
+    }
+
+// ================================ GETTERS ====================================
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+    // ================================ SETTERS ================================
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
