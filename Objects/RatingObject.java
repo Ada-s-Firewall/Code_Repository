6030ed67e18db1221134e44b3edffa5d9c45deca
Will Higdon <47529524+YaBOIWill@users.noreply.github.com>
2020-04-06 15:48:19 -0400

    created code for rating object

--- Objects/RatingObject.java
+++ Objects/RatingObject.java
@@ -1,9 +1,54 @@
 package Objects;
 
 /**
+ * This is an object for the user's rating of a particular album or song.
  *
- * @author Quinn Tjin-A-Soe Last updated: 04.02.2020
+ * @authors: Quinn Tjin-A-Soe, Will Higdon
+ * Last Updated: April 6, 2020
  */
-public class RatingObject extends RecordObject {
 
+import java.util.UUID;
+
+public class RatingObject extends RecordObject {
+    /*
+    * Variables for the user's rating.
+    */
+    private double usersRating;
+    private String spotifyID;
+    protected String uuid;
+
+    //=================  CONSTRUCTORS ===================
+
+    public RatingObject() {
+        this.setUuid(RatingObject.generateUuid());
+    }
+    public RatingObject(double _usersRating, String _spotifyID, String _uuid) {
+        this.usersRating = usersRating;
+        this.spotifyID = spotifyID;
+        this.uuid = uuid;
+    }
+
+    //=================  GETTERS ========================
+
+    public double getUsersRating() {
+        return this.usersRating;
+    }
+    public String getSpotifyId() {
+        return this.spotifyID;
+    }
+    public String getUUID() {
+        return this.uuid;
+    }
+
+    //=================  SETTERS ========================
+
+    public void setUsersRating(double _usersRating) {
+        this.usersRating = usersRating;
+    }
+    public void setSpotifyID(String _spotifyID) {
+        this.spotifyID = spotifyID;
+    }
+    public void setUUID(String _uuid) {
+        this.uuid = uuid;
+    }
 }
