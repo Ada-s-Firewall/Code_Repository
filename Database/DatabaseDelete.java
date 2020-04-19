0a3e96c4d7166f64090347be5195e63f839f6ccd
Fernando Villarreal <fvilla2457@gmail.com>
2020-04-19 18:44:08 -0400

    Additional database delete methods added

--- Database/DatabaseDelete.java
+++ Database/DatabaseDelete.java
@@ -3,10 +3,11 @@ package Database;
 /*
  * This is a Database class for deleting records.
  *
- * Last Updated: April 13, 2020
- * @author Will Higdon, Quinn Tjin-A-Soe
+ * Last Updated: April 19, 2020
+ * @author Will Higdon, Quinn Tjin-A-Soe, Fernando Villarreal
  */
 import Objects.UserObject;
+import Objects.RecordObject;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -15,6 +16,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DatabaseDelete {
+
+    //==================== PUBLIC METHODS ====================
 
     /**
      * This method deletes a specified record from the database. This method
@@ -33,19 +36,75 @@ public class DatabaseDelete {
         scanner.useDelimiter("\t");
 
         while (scanner.hasNextLine()) {
-            if (scanner.hasNext(userName) == true) {
+            String nextLine = scanner.nextLine();
+            if (nextLine.contains(userName) && nextLine.contains("" + DatabaseInterface.active)) {
+                stringRecord += _user.getUuid() + "\t";
                 stringRecord += _user.getUserName() + "\t";
                 stringRecord += _user.getUserPassword() + "\t";
                 stringRecord += _user.getUserEmail() + "\t";
-                stringRecord += _user.getName() + "\t";
+                stringRecord += _user.getUserFirstName() + "\t";
                 stringRecord += _user.getUserLastName() + "\t";
                 stringRecord += DatabaseInterface.inactive + "\n";
             } else {
-                stringRecord += scanner.nextLine() + "\n";
+                stringRecord += nextLine + "\n";
             }
         }
         BufferedWriter writer = new BufferedWriter(new FileWriter(_file, false));
         writer.write(stringRecord);
+        writer.close();
+        // Delete the user's information from the UserLogin.txt file
+        DatabaseDelete.deleteUserLogin(_user);
+    }
+
+    /**
+     * Deletes the given user from the database.
+     * @param _user
+     * @throws Exception
+     */
+    public static void deleteUserRecord(UserObject _user) throws Exception {
+        DatabaseDelete.deleteUserRecord(DatabaseInterface.userInfoFile, _user);
+    }
+
+    /**
+     * Deletes the user associated with the given username from the database.
+     * @param _username
+     * @throws Exception
+     */
+    public static void deleteUserRecord(String _username) throws IOException, Exception {
+        // Load the UserObject using the given username
+        UserObject user = DatabaseRead.readUserRecord(_username);
+        // Delete the user from the database
+        DatabaseDelete.deleteUserRecord(DatabaseInterface.userInfoFile, user);
+    }
+
+    //==================== PRIVATE METHODS ====================
+
+    /**
+     * Deletes a user's login information from the UserLogin.txt file.
+     * @param _user
+     * @throws Exception
+     */
+    private static void deleteUserLogin(UserObject _user) throws Exception {
+        // Create a scanner for the UserLogin.txt file
+        Scanner scanner = new Scanner(DatabaseInterface.userLoginFile);
+        // Create an empty string to append all of the contents of the file
+        String recordsString = "";
+        String username = _user.getUserName();
+        // Iterate over the file to find a record with the given username
+        while (scanner.hasNextLine()) {
+            String nextLine = scanner.nextLine();
+            // If the record contains the username and is active, overwrite the record as inactive.
+            if (nextLine.contains(username) && nextLine.contains("" + DatabaseInterface.active)) {
+                recordsString += username + "\t" + _user.getUserPassword() + "\t";
+                recordsString += DatabaseInterface.inactive + "\n";
+            // If the record does not contain the username, append it to recordsString without overwriting.
+            } else {
+                recordsString += nextLine + "\n";
+            }
+        }
+        // Write the recordsString to the file
+        BufferedWriter writer = new BufferedWriter(new FileWriter(DatabaseInterface.userLoginFile, false));
+        writer.write(recordsString);
         writer.close();
     }
 }
