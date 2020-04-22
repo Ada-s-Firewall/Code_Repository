9b968371461657c8ce07cbf69e47b3ce5723b28b
Quinn <qatjinas@uncg.edu>
2020-04-22 05:01:10 -0400

    modified update class

--- Database/DatabaseInterface.java
+++ Database/DatabaseInterface.java
@@ -15,10 +15,11 @@ import java.util.ArrayList;
 public interface DatabaseInterface {
 //=========================== CLASS VARIABLES ==================================
 
-    public static final File userInfoFile = new File("Datastore/UserInfo.txt");
-    public static final File userLoginFile = new File("Datastore/UserLogin.txt");
-    public static final File userRatingFile = new File("Datastore/UserRating.txt");
-    public static final File userPlanToListen = new File("Datastore/UserPlanToListen.txt");
+    public static final File userInfoFile = new File("src/Datastore/UserInfo.txt");
+    public static final File userLoginFile = new File("src/Datastore/UserLogin.txt");
+    public static final File userRatingFile = new File("src/Datastore/UserRating.txt");
+    public static final File userPlanToListen = new File("src/Datastore/UserPlanToListen.txt");
+    public static final File temporaryFile = new File("tempfile.txt");
     public static final boolean active = true;
     public static final boolean inactive = false;
 
@@ -31,21 +32,23 @@ public interface DatabaseInterface {
      */
     public abstract void createRecord(File _file, ArrayList<String> _record);
 
-
     /**
      * Creates a new user record.
+     *
      * @param _user
      */
     public void createUserRecord(UserObject _user);
 
     /**
      * Create a new user's rating record.
+     *
      * @param _rating
      */
     public void createUserRating(RatingObject _rating);
 
     /**
      * Creates a new plan-to-listen record.
+     *
      * @param _planToListen
      */
     public void createUserPlanToListen(PlanToListenObject _planToListen);
@@ -60,15 +63,38 @@ public interface DatabaseInterface {
 
     /**
      * Deletes the given user from the database.
+     *
      * @param _user
      */
     public void deleteUserRecord(UserObject _user);
 
     /**
      * Deletes the user with the associated username from the database.
+     *
      * @param _username
      */
     public void deleteUserRecord(String _username);
+
+    /**
+     *
+     * @param _user
+     * @param _newPassword
+     */
+    public abstract void updateUserPassword(UserObject _user, String _newPassword);
+
+    /**
+     *
+     * @param _user
+     * @param _newUsername
+     */
+    public abstract void updateUsername(UserObject _user, String _newUsername);
+
+    /**
+     *
+     * @param _user
+     * @param _newUserEmail
+     */
+    public abstract void updateUserEmail(UserObject _user, String _newUserEmail);
 
     /**
      * This method returns a specified record from the database.
@@ -81,20 +107,25 @@ public interface DatabaseInterface {
 
     /**
      * Read and return a UserObject using the given username.
+     *
      * @param _username
      * @return UserObject
      */
     public UserObject readUserRecord(String _username);
 
     /**
-     * Read and return a RecordObjectList containing the specified user's ratings.
+     * Read and return a RecordObjectList containing the specified user's
+     * ratings.
+     *
      * @param _username
      * @return RecordObjectList
      */
     public RecordObjectList readUsersRatings(String _username);
 
     /**
-     * Read and return a RecordObjectList containing the specified user's plan-to-listen records
+     * Read and return a RecordObjectList containing the specified user's
+     * plan-to-listen records
+     *
      * @param _username
      * @return RecordObjectList
      */
